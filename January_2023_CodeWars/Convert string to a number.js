a0e057616cf29b9a9b11ecdf23832a944cd4f25b
Zasmai <98821079+Zasmai@users.noreply.github.com>
2023-01-03 13:20:24 -0500

    Convert a string to a number

---
+++ January_2023_CodeWars/Convert string to a number.js
@@ -0,0 +1,16 @@
+var stringToNumber = function(str){
+    return (Math.floor(str));
+}
+
+// other solutions
+
+// const stringToNumber = function(str){
+//     return Number(str);
+//   }
+
+//   var stringToNumber = Number;
+
+
+//   var stringToNumber = function(str){
+//     return parseInt(str);
+//   }
