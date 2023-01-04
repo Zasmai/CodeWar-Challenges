0b666005c18a4f48712de68052453abd9c8a745a
Zasmai <98821079+Zasmai@users.noreply.github.com>
2023-01-04 11:45:19 -0500

    Convert boolean values to strings
    Convert boolean values to strings "Yes" or "No".

---
+++ Jan04 CodeWars/Convert boolean values to strings yes or no.js
@@ -0,0 +1,28 @@
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+
+function boolToWord( bool ){
+    let result;
+    if (bool == true) {
+        return "Yes";
+    } else {
+        return "No";
+    }
+}
+
+
+// function boolToWord( bool ){
+//     return bool ? 'Yes':'No';
+//   }
+
+// function boolToWord( bool ){
+//     if (bool) {
+//       return 'Yes';
+//     } else {
+//       return 'No';
+//     }
+//   }
+
+// let boolToWord = bool => bool ? 'Yes' : 'No';
+
+// const boolToWord = bool => bool ? 'Yes' : 'No';
