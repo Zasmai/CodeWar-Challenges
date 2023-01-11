35786f3e55d3a38b9ff0ba8ca63ab10d2cd58ef1
Zasmai <98821079+Zasmai@users.noreply.github.com>
2023-01-11 18:03:59 -0500

    Submit daily Codewar challgnge

---
+++ January_2023_CodeWars/Jan11-Codewars/YouCantCodeUnderPressure.js
@@ -0,0 +1,6 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
