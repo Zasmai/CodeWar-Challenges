82847f3b0d3910b1b36ffa25ba533cfb4bd447df
Sarah Codes <iamsazzy@gmail.com>
2023-01-30 16:28:34 -0500

    Add files via upload

---
+++ January_2023_CodeWars/Jan29-Codewars/thirdAngleOfATriangle.js
@@ -0,0 +1,9 @@
+
+// P - the paramaters are a, b. the known angles of a triangle. use otherAngle to find the answer
+// R - return a the equation for otherAngle
+// E - assert.strictEqual(otherAngle(30, 60), 90);
+// P - we know that 180 - a + b will give us the third angle and thus the answer. 
+
+function otherAngle(a, b) {
+  let otherAngle = 180 - (a + b);
+  return otherAngle;
